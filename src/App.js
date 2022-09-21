5b846fa9f7850f2aecd889c32899c0528fee0df6
Dominika <fud.dominika@gmail.com>
2022-09-21 12:42:45 +0200

    setup files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './app.module.scss';
+import Setup from 'components/Setup/Setup'
+
+const App = () => (
+    <div>
+        <Setup />
+    </div>
+)
+
+export default App;
---
+++ src/components/Setup/Setup.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from './setup.module.scss';
+
+const Setup = () => (
+    <p className={styles.setup}>Setup text</p>
+)
+
+export default Setup;
